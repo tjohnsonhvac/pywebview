be06e9319e604da89f725b70f76f8c9833b77ef8
Roman Sirokov <roman.sirokov@siili.com>
2018-02-23 14:36:38 +0200

    [WinForms] Don't suppress JS error messages in debug mode

--- webview/winforms.py
+++ webview/winforms.py
@@ -81,12 +81,12 @@ class BrowserView:
 
             self.web_browser = WinForms.WebBrowser()
             self.web_browser.Dock = WinForms.DockStyle.Fill
-            self.web_browser.ScriptErrorsSuppressed = True
+
             self.web_browser.WebBrowserShortcutsEnabled = False
             self.web_browser.DpiAware = True
 
-            if not debug:
-                self.web_browser.IsWebBrowserContextMenuEnabled = False
+            self.web_browser.ScriptErrorsSuppressed = not debug
+            self.web_browser.IsWebBrowserContextMenuEnabled = debug
 
             self.js_result_semaphor = threading.Semaphore(0)
             self.js_bridge = BrowserView.JSBridge()
